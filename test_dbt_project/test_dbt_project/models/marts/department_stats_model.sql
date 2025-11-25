bce466e80fde91b1e645e2d62342ffe2df002d1f
Alexander Del <olalso2007@gmail.com>
2025-11-25 13:57:57 +0300

    Minor fixes

--- test_dbt_project/test_dbt_project/models/marts/department_stats_model.sql
+++ test_dbt_project/test_dbt_project/models/marts/department_stats_model.sql
@@ -1,18 +1,21 @@
+{%- set query -%}
+    select distinct department from {{ source('raw', 'departments') }}
+{%- endset -%}
+{%- set result = run_query(query) -%}
+{%- if result -%}
+    {%- set departments = result.columns[0].values() -%}
+{%- else -%}
+    {%- set departments = [] -%}
+{%- endif -%}
+
 with department_stats as (
     select
         order_hour_of_day,
-        {%- set query -%}
-            select distinct department from {{ source('raw', 'departments') }}
-        {%- endset -%}
-        {%- set result = run_query(query) -%}
-        {%- if result -%}
-            {%- set departments = result.columns[0].values() -%}
-        {%- else -%}
-            {%- set departments = [] -%}
-        {%- endif -%}
         {% for department in departments %}
             sum(case when department = '{{ department }}' then product_cnt else 0 end) as "{{ department }}"
-            {%- if not loop.last %},{% endif %}
+            {%- if not loop.last %}
+                ,
+            {% endif %}
         {% endfor %}
     from {{ ref('order_department_count_model') }}
     group by 1)
