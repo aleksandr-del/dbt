7159e79656bfe32a1ad69dc9bd2cdf2326a4ff0c
Alexander Del <olalso2007@gmail.com>
2025-12-02 14:02:03 +0300

    Incremental model

---
+++ test_dbt_project/test_dbt_project/models/staging/stg_incremental_orders.sql
@@ -0,0 +1,21 @@
+
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='append'
+    )
+}}
+
+select
+    o.order_id,
+    o.user_id,
+    o.order_number
+from {{ source('raw', 'orders') }} o
+where
+    1 = 1
+{% if not is_incremental() %}
+    and o.order_number = 1
+{% endif %}
+{% if is_incremental() %}
+    and o.order_number = (select max(order_number) + 1 from {{ this }} )
+{% endif %}
